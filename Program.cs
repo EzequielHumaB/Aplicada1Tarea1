2ed2a4733adce92c6b56658a1e14a47e422837d9
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-05-11 20:07:04 -0400

    Esta es la tarea 1 de Programacion Aplicada 1, donde se tienen los ejercicios de los capítulos 1-4

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aplicada1Tarea1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            // Primer ejecicio del capitulo 1
+            Console.WriteLine("Ezequiel Huma Brito");
+
+            // Quinto ejercicio del capitulo 5
+            Console.WriteLine("Crispr es una tecnica de edicion genetica que se basa en la forma en que se defienden las bacterias");
+            Console.WriteLine("Alemania es el pais mas rico de Europa");
+            Console.WriteLine("Hernan Cortes fue colonizador de lo que hoy es Peru y Chile");
+
+        }
+    }
+}

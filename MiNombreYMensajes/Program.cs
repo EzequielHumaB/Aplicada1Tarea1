a391b6da63fb2044501c4dd332f91c56aeb90733
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-05-11 20:21:13 -0400

    Agregacion de una clase donde pongo mi nombre y unos cuantos mensajes mas (Ejercicio 1, Capitulo 1)

---
+++ MiNombreYMensajes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MiNombreYMensajes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ezequiel Huma Brito");
+
+            //Mensajes adicionales
+            Console.WriteLine("Crispr es una herramienta de edidicion genetica");
+            Console.WriteLine("Haruki Murakami no ha ganado el premio Nobel de la Paz");
+        }
+    }
+}
